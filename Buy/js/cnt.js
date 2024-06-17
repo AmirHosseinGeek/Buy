f474175316a335fa300d541f33ca062eb208024b
AmirHosseinGeek <iloveeg85@gmail.com>
2024-06-17 16:38:42 +0330

    Add files via upload

---
+++ Buy/js/cnt.js
@@ -0,0 +1,23 @@
+console.log(IDcard)
+console.log(password)
+console.log(CVV2)
+console.log(name)
+console.log(phone)
+console.log(age)
+console.log(destinationcard)
+console.log(message)
+
+
+
+function Clickbtn(){
+    alert('آیا تمامیه اطلاعات تان درست درج شده است؟')
+    document.write('<p dir="rtl">عملیات موفقیت آمیز بود</p>')
+} 
+
+console.log('شماره کارت', shomareh)
+console.log('رمز', ramz)
+console.log('سی وی وی ۲', matn)
+console.log('+98', shomareh1)
+console.log('نام', namE)
+console.log('سن', sen)
+console.log('مقصد', maghsad)
